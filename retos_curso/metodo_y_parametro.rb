a4c42f8ce21a5eb900c9275adcca721c2aafe698
narguello8462 <nestor.arguello@gmail.com>
2016-09-05 21:41:08 -0400

    Update class/home

--- retos_curso/metodo_y_parametro.rb
+++
@@ -1,9 +0,0 @@
-
-def recibe_proc(procedimiento, &bloque)
-  bloque.call(procedimiento)
-end
-
-num = gets.chomp
-procedimiento = Proc.new {|n| n * num}
-
-recibe_proc(procedimiento) {|proced| puts proced * proced}
