b88a1835de74ff4de0c6b40974803f7b5b0f4b16
narguello8462 <nestor.arguello@gmail.com>
2016-09-23 05:18:16 -0400

    Rails autodidact new projects

--- rails_autodidact/europe/db/schema.rb
+++ rails_autodidact/europe/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916034054) do
+ActiveRecord::Schema.define(version: 20160922231312) do
 
   create_table "countries", force: :cascade do |t|
     t.string   "name"
--- rails_autodidact/jukebox/db/schema.rb
+++ rails_autodidact/jukebox/db/schema.rb
@@ -10,21 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916225314) do
+ActiveRecord::Schema.define(version: 20160923045821) do
 
-  create_table "authors", force: :cascade do |t|
-    t.integer  "book_id"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_authors_on_book_id"
-  end
-
-  create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "albums", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "release_year"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
