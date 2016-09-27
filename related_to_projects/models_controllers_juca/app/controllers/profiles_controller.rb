fa3aee0c8d637cbf83ead67eeed4244d9064e50e
Néstor <nestor.arguello@gmail.com>
2016-09-27 17:00:19 -0400

    modified models_controllers_juca

--- related_to_projects/models_controllers_juca/app/controllers/profiles_controller.rb
+++ related_to_projects/models_controllers_juca/app/controllers/profiles_controller.rb
@@ -1,11 +1,25 @@
 class ProfilesController < ApplicationController
   def index
+    puts params.inspect
+    render json: Profile.all
+
   end
 
   def create
+    @person = Person.find(params[:person_id])
+    @prof = Profile.new(profile_params)
+    @prof.person = @person
+
+    if @prof.save
+      render json: @prof
+    else
+      render json: {"errors": @prof.errors, "code": 500}
+    end
+
   end
 
   def show
+    
   end
 
   def update
@@ -13,4 +27,11 @@ class ProfilesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def profile_params
+    params.permit(:dni, :name, :lastname, :birthday, :gender, :car)
+  end
+
 end
