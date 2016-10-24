a1a73e4da8a7feaa1c355fc6ec828619825d8c27
narguello8462 <nestor.arguello@gmail.com>
2016-10-24 01:23:38 -0400

    added some minor changes on rails-autodidact

--- rails_autodidact/shop/app/models/product.rb
+++ rails_autodidact/shop/app/models/product.rb
@@ -1,4 +1,2 @@
 class Product < ApplicationRecord
-  has_many :line_items
-  has_many :orders, through: :line_items
 end
--- rails_autodidact/shop/db/migrate/20160924225907_add_columns_to_product.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToProduct < ActiveRecord::Migration[5.0]
-  def change
-    add_column :products, :weight, :integer
-    add_column :products, :in_stock, :boolean
-    add_column :products, :expiration_date, :date
-  end
-end
