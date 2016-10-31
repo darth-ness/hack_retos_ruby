75b5886448103679101752ff368e1c7777efff59
Néstor <nestor.arguello@gmail.com>
2016-10-31 16:40:43 -0400

    added jquery/sass class, by angel

---
+++ en_clases/javascript/jquery/js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+
+  $('a').click(function() {
+    $('img').toggle('slow');
+  })
+
+  // $('a').click(function () {
+  //   if ($('img').attr('src') === 'img/logo1.jpeg') {
+  //     $('img').attr('src', 'img/logo2.jpeg');
+  //   } else {
+  //     $('img').attr('src', 'img/logo1.jpeg');
+  //   }
+  // })
+
+});
